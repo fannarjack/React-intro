0ea34193f96a7b9970672813636b8777ac0dffc0
Fannar <fannarjack@gmail.com>
2024-10-21 14:05:50 +0000

    asdf

---
+++ src/hamburger.tsx
@@ -0,0 +1,14 @@
+import "./hamburger.css"
+function Btn() {
+
+  return(
+  <div className="container">
+    <button className="btn">
+      Hey, how are you?
+    </button>
+  </div>
+  )
+  
+}
+
+export default Btn
